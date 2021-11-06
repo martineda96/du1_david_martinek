1e70cf62ef358987d39097d66e89723c73245ba6
martineda96 <martineda@natur.cuni.cz>
2021-11-06 11:18:48 +0100

    Added h1 tag

--- du1_david_martinek/du1.py
+++ du1_david_martinek/du1.py
@@ -11,19 +11,19 @@ y = int(input("Zadejte počet řádků hracího pole:"))
 while y < 1:
     y = int(input("Počet řádků musí být kladné číslo, zkuste to znovu:"))
 
-strana_ctverce = 60
+STRANA_CTVERCE = 60
 speed(0)
 #Kreslení čtvercové mřížky prostřednictvím tří for cyklů. První for cyklus udává počet řádků.
 #Druhý cyklus udává počet sloupců a třetí cyklus (vnitřní) kreslí jednotlivé čtverce.
 for _ in range(y):
     for _ in range (x):
         for _ in range(4):
-            forward(strana_ctverce)
+            forward(STRANA_CTVERCE)
             left(90)
-        forward(strana_ctverce)
-    backward(x*strana_ctverce)
+        forward(STRANA_CTVERCE)
+    backward(x*STRANA_CTVERCE)
     left(90)
-    forward(strana_ctverce)
+    forward(STRANA_CTVERCE)
     right(90)
 
 #volna_policka udává, kolik mohou hráči dohromady odehrát tahů. Po nakreslení každého symbolu jejich počet klesne o 1.
@@ -37,14 +37,14 @@ while volna_policka != 0:
     while radek < 1 or radek > y:
         radek = int(input("Takový řádek neexistuje, zkus to znovu:"))
     penup()
-    setpos(sloupec*strana_ctverce-strana_ctverce/2,radek*strana_ctverce-strana_ctverce/2)
+    setpos(sloupec*STRANA_CTVERCE-STRANA_CTVERCE/2,radek*STRANA_CTVERCE-STRANA_CTVERCE/2)
     pendown()
     pencolor("blue")
     right(45)
     for _ in range(2):
-        forward(strana_ctverce/2)
-        backward(strana_ctverce)
-        forward(strana_ctverce/2)
+        forward(STRANA_CTVERCE/2)
+        backward(STRANA_CTVERCE)
+        forward(STRANA_CTVERCE/2)
         right(90)
     right(135)
     volna_policka = volna_policka-1
@@ -56,10 +56,10 @@ while volna_policka != 0:
         while radek < 1 or radek > y:
             radek = int(input("Takový řádek neexistuje, zkus to znovu:"))
         penup()
-        setpos(sloupec*strana_ctverce-strana_ctverce/2,radek*strana_ctverce-strana_ctverce*0.9)
+        setpos(sloupec*STRANA_CTVERCE-STRANA_CTVERCE/2,radek*STRANA_CTVERCE-STRANA_CTVERCE*0.9)
         pendown()
         pencolor("red")
-        circle(strana_ctverce*0.4)
+        circle(STRANA_CTVERCE*0.4)
         volna_policka = volna_policka - 1
 
 print("Konec hry. Pokud jste hráli správně, tak v mřížce nezůstalo žádné volné políčko.")
